3b6c38886be4b3d19db1ac5306cf263e7bda4a21
Arya Krishna <dr.aryakris@outlook.com>
2022-03-08 23:34:49 -0800

    logout

--- public/logout.js
+++ public/logout.js
@@ -1,24 +1,39 @@
 //instructor demo. Modify for our project
 
-const logout = async () => {
-  try{
-    const response = await fetch('/api/users/logout', {
-      method: 'POST',
-      // body: {},
-      headers: { 'Content-Type': 'application/json' },
-    });
-  const result = await response.json();
-  console.log(result);
-    if (response.ok) {
-      document.location.replace('/login');
-    } else {
-      alert(response.statusText);
-    }
-  }
-  catch(err){
-    console.log(err);
-  }
-  };
+// const logout = async () => {
+//   try{
+//     const response = await fetch('/api/users/logout', {
+//       method: 'POST',
+     
+//       headers: { 'Content-Type': 'application/json' },
+//     });
+//   const result = await response.json();
+//   console.log(result);
+//     if (response.ok) {
+//       document.location.replace('/login');
+//     } else {
+//       alert(response.statusText);
+//     }
+//   }
+//   catch(err){
+//     console.log(err);
+//   }
+//   };
   
-  document.querySelector('#logout').addEventListener('click', logout);
+//   document.querySelector('#logout').addEventListener('click', logout);
+
+const logout = async () => {
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+};
+
+document.querySelector('#logout').addEventListener('click', logout);
   
